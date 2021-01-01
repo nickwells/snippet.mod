13307070c947dcbd02e686f8775bf4cf76581881
Nick Wells <wells.nicholas@gmail.com>
2021-01-01 01:05:44 +0000

    Fix a typo

--- snippet/doc.go
+++ snippet/doc.go
@@ -4,6 +4,6 @@ snippet is a file containing a fragment of Go code suitable to be copied into
 a larger program. The snippet files are annotated with semantic comments to
 provide documentation about the correct usage of the snippet. These semantic
 comments can also be interpreted to provide some checking of the correct
-usage
+usage.
 */
 package snippet
